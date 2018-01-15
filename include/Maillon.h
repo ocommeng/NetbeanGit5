5f9bb0a01de9f290b3d130c5ba220d12604c4031
snir <snir@ocommeng-HP-ProBook-450-G2>
2018-01-15 23:13:32 +0100

    AJout de la classe Maillon

---
+++ include/Maillon.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Maillon.h
+ * Author: snir
+ *
+ * Created on 15 janvier 2018, 23:11
+ */
+
+#ifndef MAILLON_H
+#define MAILLON_H
+
+class Maillon {
+public:
+    Maillon();
+    Maillon(const Maillon& orig);
+    virtual ~Maillon();
+private:
+
+};
+
+#endif /* MAILLON_H */
+
---
+++ src/Maillon.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Maillon.cpp
+ * Author: snir
+ * 
+ * Created on 15 janvier 2018, 23:11
+ */
+
+#include "include/Maillon.h"
+
+Maillon::Maillon() {
+}
+
+Maillon::Maillon(const Maillon& orig) {
+}
+
+Maillon::~Maillon() {
+}
+
