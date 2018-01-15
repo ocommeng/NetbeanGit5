f6ba5e3fd397dda271ab95e582c89b28abeefc8e
snir <snir@ocommeng-HP-ProBook-450-G2>
2018-01-15 23:58:00 +0100

    Ajout de la classe Marteau

---
+++ src/Marteau.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Marteau.cpp
+ * Author: snir
+ * 
+ * Created on 15 janvier 2018, 23:57
+ */
+
+#include "include/Marteau.h"
+
+Marteau::Marteau() {
+}
+
+Marteau::Marteau(const Marteau& orig) {
+}
+
+Marteau::~Marteau() {
+}
+
