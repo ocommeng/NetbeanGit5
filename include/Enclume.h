9de0995a80a1df6deeb9908ae465e6c983f93a0c
snir <snir@ocommeng-HP-ProBook-450-G2>
2018-01-15 23:24:40 +0100

    Résolution de conflit

---
+++ include/Enclume.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Enclume.h
+ * Author: snir
+ *
+ * Created on 15 janvier 2018, 23:12
+ */
+
+#ifndef ENCLUME_H
+#define ENCLUME_H
+
+class Enclume {
+public:
+    Enclume();
+    Enclume(const Enclume& orig);
+    virtual ~Enclume();
+private:
+
+};
+
+#endif /* ENCLUME_H */
+
---
+++ src/Enclume.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Enclume.cpp
+ * Author: snir
+ * 
+ * Created on 15 janvier 2018, 23:12
+ */
+
+#include "include/Enclume.h"
+
+Enclume::Enclume() {
+}
+
+Enclume::Enclume(const Enclume& orig) {
+}
+
+Enclume::~Enclume() {
+}
+
