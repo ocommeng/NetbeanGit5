e5f06d4f252aa9a74782c11069ebcaf715dc413c
snir <snir@ocommeng-HP-ProBook-450-G2>
2018-01-15 22:18:25 +0100

    Ajout d'un cout dans le main

---
+++ include/Test.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Test.h
+ * Author: snir
+ *
+ * Created on 15 janvier 2018, 22:18
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+class Test {
+public:
+    Test();
+    Test(const Test& orig);
+    virtual ~Test();
+private:
+
+};
+
+#endif /* TEST_H */
+
---
+++ src/Test.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Test.cpp
+ * Author: snir
+ * 
+ * Created on 15 janvier 2018, 22:18
+ */
+
+#include "Test.h"
+
+Test::Test() {
+}
+
+Test::Test(const Test& orig) {
+}
+
+Test::~Test() {
+}
+
