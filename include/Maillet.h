6bd5ae3ee987dc64ea1f7ae2186f93bd6733a620
snir <snir@ocommeng-HP-ProBook-450-G2>
2018-01-15 23:59:37 +0100

    Ajout de la classe Maillet

---
+++ include/Maillet.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Maillet.h
+ * Author: snir
+ *
+ * Created on 15 janvier 2018, 23:59
+ */
+
+#ifndef MAILLET_H
+#define MAILLET_H
+
+class Maillet {
+public:
+    Maillet();
+    Maillet(const Maillet& orig);
+    virtual ~Maillet();
+private:
+
+};
+
+#endif /* MAILLET_H */
+
---
+++ src/Maillet.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Maillet.cpp
+ * Author: snir
+ * 
+ * Created on 15 janvier 2018, 23:59
+ */
+
+#include "include/Maillet.h"
+
+Maillet::Maillet() {
+}
+
+Maillet::Maillet(const Maillet& orig) {
+}
+
+Maillet::~Maillet() {
+}
+
