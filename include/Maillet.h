d00a927b53ac22ca3c0a5ef252b67405c8cb474f
ocommeng <olivier-alain.commenge@ac-toulouse.fr>
2018-01-16 01:52:24 +0100

    COmmit de la nouvelle branche avantMaillet

---
+++ include/Maillet.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Maillet.h
+ * Author: ocommeng
+ *
+ * Created on 16 janvier 2018, 01:50
+ */
+
+#ifndef MAILLET_H
+#define MAILLET_H
+
+class Maillet {
+public:
+    Maillet();
+    Maillet(const Maillet& orig);
+    virtual ~Maillet();
+private:
+
+};
+
+#endif /* MAILLET_H */
+
---
+++ src/Maillet.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Maillet.cpp
+ * Author: ocommeng
+ * 
+ * Created on 16 janvier 2018, 01:50
+ */
+
+#include "Maillet.h"
+
+Maillet::Maillet() {
+}
+
+Maillet::Maillet(const Maillet& orig) {
+}
+
+Maillet::~Maillet() {
+}
+
